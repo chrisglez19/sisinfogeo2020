ccb381aefb91ceafe719aef808853ff70f0c1d78
chrisglez19 <34153212@uaz.edu.mx>
2020-01-30 11:51:20 -0600

    triang

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bas, altura, area;
+            Console.WriteLine("Programa que calcula el area de un triangulo");
+
+            Console.WriteLine("Dame la base : ");
+            bas= double.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura: ");
+            altura= double.Parse(Console.ReadLine());
+
+            area = (bas * altura)/2;
+
+            Console.WriteLine($"El area del triangulo es {area}");
+            Console.ReadLine();
+        }
+    }
+}
