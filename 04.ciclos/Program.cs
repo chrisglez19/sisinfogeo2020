017d8529ea0023fdb21f6951d8e65c1a787dfea3
chrisglez19 <34153212@uaz.edu.mx>
2020-02-04 11:44:52 -0600

    va de new

---
+++ 04.ciclos/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _04.ciclos
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int op, c=0, suma=0;
+            if (args.Length==0) Menu();
+
+            op= int.Parse(args[0]);
+
+            Console.Clear();
+            switch (op)
+            {
+                case 1: {
+                    c=1;
+                    suma=0;
+                    while(c<=100){
+                        Console.Write($"{c} ");
+                        suma+=c;
+                        c++;
+                    }
+                    Console.WriteLine($"La suma es {suma}\n\n");
+                } break;
+                case 2: {
+                    c=100;
+                    suma=0;
+                    while(c>=1){
+                        Console.Write($"{c} ");
+                        suma+=c;
+                        c--;
+                    }
+                    Console.WriteLine($"La suma es {suma}\n\n");
+                } break;
+                case 3: {
+                    suma=0;
+                    for(c=50; c<=200; c++){
+                        Console.Write($"{c} ");
+                        suma+=c;
+                    }
+                    Console.WriteLine($"La suma es {suma}\n\n");
+                } break;
+                case 4: {
+                    suma=0;
+                    for(c=2; c<=100; c+=2){
+                        Console.Write($"{c} ");
+                        suma+=c;
+                    }
+                    Console.WriteLine($"La suma es {suma}\n\n");
+                } break;
+                case 5: {
+                    suma=0;
+                    for(c=99; c>=1; c-=2){
+                        Console.Write($"{c} ");
+                        suma+=c;
+                    }
+                    Console.WriteLine($"La suma es {suma}\n\n");
+                } break;
+                case 6: {
+                    c=272;
+                    suma=0;
+                    while(c>=40){
+                        Console.Write($"{c}");
+                        suma+=c;
+                        c-=4;
+                    }
+                    Console.WriteLine($"La suma es {suma}\n\n");
+                } break;
+                default:Console.WriteLine("Opcion Invalida");break;
+            }
+            return 0;
+        }
+
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("Programa sobre el uso de estructuras de control\n\n");
+            Console.WriteLine("[1] Números del 1 al 100 con ciclo while");
+            Console.WriteLine("[2] Números del 100 al 1 con ciclo do .. while");
+            Console.WriteLine("[3] Números del 50 al 200 con ciclo for");
+            Console.WriteLine("[4] Números del 2 al 100 solo los pares con ciclo for");
+            Console.WriteLine("[5] Números del 99 al 1 solo los impares con ciclo for");
+            Console.WriteLine("[6] Números del 272 al 40 en decrementos de 4 con ciclo while");
+            
+        }
+    }
+}
---
+++ 05.pizzas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _05.pizzas
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length==0){
+                Menu();
+                return 1;
+            }
+
+            char tam = char.Parse(args[0]);
+            string[] ings = args[1].Split("+");
+            char cub = char.Parse(args[2]);
+            char don = char.Parse(args[3]);
+
+            string tamano= "", ingredientes ="", cubierta="", donde ="";
+
+            if(tam=='P') tamano = "Pequeña";
+            else if (tam=='M') tamano="Mediana";
+            else tamano = "Grande";
+
+            foreach(string i in ings){
+                switch(char.Parse(i))
+                {
+                    case 'E': ingredientes+= "Extra Queso, "; break;
+                    case 'C': ingredientes+= "Champinones, "; break;
+                    case 'T': ingredientes+= "Tomate, "; break;
+                    case 'P': ingredientes+= "Pina, "; break;
+                }
+            }
+
+            if(cub=='D') cubierta = "Delgada"; else cubierta="Gruesa";
+            if(don=='C') donde = "Comer aqui"; else donde="Para llevar";
+
+            Console.Clear();
+            Console.WriteLine($"Tamaño      :{tamano}");
+            Console.WriteLine($"Ingredientes:{ingredientes}");
+            Console.WriteLine($"Cubierta    :{cubierta}");
+            Console.WriteLine($"Donde comer :{donde} \n\n");
+            
+            
+
+            return 0;
+        }
+
+        
+
+        static void Menu() {
+
+            Console.WriteLine("Procesando Pizza");
+            Console.WriteLine("Tamaño       :(P)equeña, (M)ediana o (G)rande");
+            Console.WriteLine("Ingredientes : (E)xtra Queso, (Ch)ampiñones, (T)omates, (P)iña");
+            Console.WriteLine("Cubierta     : (D)elgada o (G)ruesa");
+            Console.WriteLine("Donde comer  : (C)omer aquí o (P)ara Llevar.\n\n");
+           
+            
+        }
+
+    }
+}
