bbede2644a6b20e4a8095ce34e65473aa27c334e
chrisglez19 <34153212@uaz.edu.mx>
2020-03-17 11:51:54 -0600

    17Marzo

---
+++ 21.Linq1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _21.Linq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //fuente de datos
+            int[] numeros = new int [] {
+                10,20,30,40,50,60,70,80,90,100,
+                -10,-20,0,1000,2000,3000,4000,5000,
+                10,20,30,40,50,60,70,80,90,101,
+                -10,-20,0,1000,2000,3000,4000,5000
+            };
+
+            //Consulta 1, paraes, regresa un IENUMERABLE<int>
+            var consulta =
+                from num in numeros
+                where num%2==0
+                select num;
+
+            //Ejecutr consulta
+            foreach (var num in consulta)
+                Console.Write($"{num} ");
+            
+            //Consulta 2, numeroes entre 10 y 200, ni ceros en un arreglo regresa un int[]
+            var consulta2 = 
+                (from num in numeros 
+                where num>=10 && num<=200 
+                select num).ToArray();
+
+            Console.WriteLine();
+
+            Console.WriteLine("\nNumeros mayores a 100\n");
+            for (int i=0; i<consulta2.Count(); i++)
+                Console.WriteLine($"{consulta2[i]} ");
+
+            //Consulta 3, negativvos en una lista
+            var consulta3 =
+                (from num in numeros
+                where num>0
+                select  num).ToList();
+
+            Console.WriteLine("\nNumeros negativvos:\n");
+            consulta3.ForEach(num=>Console.WriteLine(num));
+        }
+    }
+}
---
+++ 23.Linq3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _23.Linq3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Estudiante> grupo =  new List<Estudiante>();
+
+            grupo.Add( 
+                new Estudiante {Id=1234,Nombre="Jose",
+                Ciudad="Zacatecas", Calle = "1A de Mayo",
+                Cals=  new List<int> {90,80,70,60}});
+
+            grupo.Add( 
+                new Estudiante {Id=3421,Nombre="Maria",
+                Ciudad="Fresnillo", Calle = "Benito",
+                Cals=  new List<int> {90,80,70,60}});
+
+            grupo.Add( 
+                new Estudiante {Id=2222,Nombre="Miguel",
+                Ciudad="Jerez", Calle = "Garcia Salinaz",
+                Cals=  new List<int> {50,50,50,40}});
+
+            grupo.Add( 
+                new Estudiante {Id=6666,Nombre="Lourdes",
+                Ciudad="Zacatecas", Calle = "El Vergel",
+                Cals=  new List<int> {100,100,100,90}});   
+
+            //Imprime los datos
+            Console.WriteLine("\nTodo de todo\n");
+            grupo.ForEach(Estudiante=>Console.WriteLine(Estudiante.ToString())); 
+
+            var q1 = ( 
+                from est in grupo
+                orderby est.Nombre
+                select est).ToList();
+            
+            Console.WriteLine("\nTodo ordenado por nombre: \n");
+            q1.ForEach(est=>Console.WriteLine(est.ToString()));
+
+            //Nadamas los de zacatecas
+            var q2 = ( 
+                from est in grupo
+                where est.Ciudad=="Zacatecas"
+                select est).ToList();
+            
+            Console.WriteLine("\nNadamas los de Zac:\n");
+            q2.ForEach(est=>Console.WriteLine(est.ToString()));
+
+            //Estudiantes promedio mayor a 70
+            var q3 = ( 
+                from est in grupo
+                where est.Cals.Average()>70
+                select new {nombre=est.Nombre, promedio=est.Cals.Average() }
+                ).ToList();
+            
+            Console.WriteLine("\nNombre y promedio de los mayores a 70 \n");
+            q3.ForEach(est=>Console.WriteLine($"Nombre:{est.nombre}, Promedio:{est.promedio}"));
+
+            //Agrupar por ciudad
+            var q4 = 
+                from est in grupo
+                group est by est.Ciudad;
+                
+            Console.WriteLine("\nTodo ordenado por nombre: \n");
+            foreach(var ciudad in q4){
+                Console.WriteLine(ciudad.Key);
+                foreach(var est in ciudad)
+                Console.WriteLine(est.ToString());
+            }
+        }
+    }
+}
